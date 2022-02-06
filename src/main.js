541776eac29f31b817967fe5b2888a8206ef1e13
Ktaewon <dydalehdxo@naver.com>
2022-02-06 19:27:24 +0900

    chat site

---
+++ src/main.js
@@ -0,0 +1,92 @@
+// Template Engine: Pug
+// CSS framework : TailwindCSS
+
+const Koa = require('koa');
+const Pug = require('koa-pug');
+const path = require('path');
+const serve = require('koa-static');
+const mount = require('koa-mount');
+const route = require('koa-route');
+const websockify = require('koa-websocket');
+const mongoClient = require('./mongo');
+
+const app = websockify(new Koa());
+
+const pug = new Pug({
+  viewPath: path.resolve(__dirname, './views'),
+  app: app, // Binding `ctx.render()`, equals to pug.use(app)
+});
+// tailwindCSS 정적 경로 설정
+app.use(mount('/dist', serve(__dirname + '/../dist')));
+// static 파일 내려주기
+app.use(mount('/public', serve('src/public')));
+
+app.use(async (ctx) => {
+  await ctx.render('main');
+});
+
+const _client = mongoClient.connect();
+
+async function getChatsCollection() {
+  const client = await _client;
+  return client.db('chat').collection('chats');
+}
+
+// Using routes
+app.ws.use(
+  route.all('/ws', async (ctx) => {
+    const chatsCollection = await getChatsCollection();
+    const chatsCursor = chatsCollection.find(
+      {},
+      {
+        sort: {
+          createdAt: 1,
+        },
+      }
+    );
+
+    const chats = await chatsCursor.toArray();
+    ctx.websocket.send(
+      JSON.stringify({
+        type: 'sync',
+        payload: {
+          chats,
+        },
+      })
+    );
+
+    ctx.websocket.on('message', async (data) => {
+      if (typeof data !== 'string') {
+        return;
+      }
+      const chat = JSON.parse(data);
+
+      chatsCollection.insertOne({
+        ...chat,
+        createdAt: new Date(),
+      });
+
+      const { message, nickname } = chat;
+
+      const { server } = app.ws;
+
+      if (!server) {
+        return;
+      }
+
+      server.clients.forEach((client) => {
+        client.send(
+          JSON.stringify({
+            type: 'chat',
+            payload: {
+              nickname,
+              message,
+            },
+          })
+        );
+      });
+    });
+  })
+);
+
+app.listen(5000);
